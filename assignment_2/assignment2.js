f51c503b4a5a64c1e709b24dc864d9e79dcff71b
unknown <pmonvillers@hotmail.fr>
2017-12-12 18:15:01 -0500

    exercices

--- assignment_2/assignment2.js
+++ assignment_2/assignment2.js
@@ -1,10 +1,18 @@
 // Exercise #1
 // Write your solution below
 
+for (var triangle = "#"; triangle.length <= 7; triangle += "#")
+
+console.log(triangle);
+    
+
+
 
 /* 
 Write your explanation for Exercise #1 here
-
+I declare my variable by a starting value (here triangle = "#")
+then evaluates a condition  (here I don't want that the length of the string be longer than 7)
+then  increments the variable after every iteration of the loop (here one more "#")
 */
 
 
@@ -13,8 +21,21 @@ Write your explanation for Exercise #1 here
 
 // Exercise #2
 // Write your solution below
-
-
+function evenOrOdd(number){
+    
+     if(number % 2){
+    
+      console.log('the number is even');
+    
+     }
+     else{
+    
+      console.log('the number is odd');
+    
+     }
+    
+    }
+console.log(number)
 /* 
 Write your explanation for Exercise #2 here
 
